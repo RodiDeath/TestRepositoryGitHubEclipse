d14eb1cabd92d170bccf28dad06699420eae8c5d
U0180840 <pnovoa@viewnext.com>
2019-03-15 11:21:56 +0100

    Añadida nuevo sysout

---
+++ TestJavaProject/src/testjava/test/Test.java
@@ -0,0 +1,14 @@
+package testjava.test;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hola mundo");
+		System.out.println("Prueba");
+	}
+
+}
